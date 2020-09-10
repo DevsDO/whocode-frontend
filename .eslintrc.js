c590bb0d86d599eaf04c0192d9bd9672128da6bc
nathaly <nathzzpersia@gmail.com>
2020-09-10 13:15:29 -0400

    configured husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,21 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
+    jest: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
-  plugins: ['react', 'prettier'],
+  extends: ["plugin:react/recommended", "airbnb", "prettier", "prettier/react"],
+  plugins: ["react", "prettier"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: "module",
   },
-  rules: {}
-};
+  rules: {},
+}
