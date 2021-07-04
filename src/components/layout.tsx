36aeb3508b387e52b0c18855a67eb89cf81673b3
JhonasV <jhonas@gmail.com>
2021-07-03 23:40:13 -0400

    feat: header siteTitle prop value updated

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,30 +5,20 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import { ThemeProvider } from "styled-components"
-import Header from "./header"
-import "./layout.css"
-import theme from "../theme"
-import GlobalStyles from "../globalStyles"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import { ThemeProvider } from "styled-components";
+import Header from "./header";
+import "./layout.css";
+import theme from "../theme";
+import GlobalStyles from "../globalStyles";
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle={"DominicanWho.Codes"} />
       <div
         style={{
           margin: `0 auto`,
@@ -44,11 +34,11 @@ const Layout = ({ children }) => {
         </footer>
       </div>
     </ThemeProvider>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
