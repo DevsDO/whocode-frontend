1e08a95e51971a35af9cc8f6d917c4ad2ef0665b
JhonasV <jhonas@gmail.com>
2021-07-04 01:47:36 -0400

    move react import to the file's first line

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,5 @@
-import { Link } from "gatsby";
-
 import React, { useState, FC } from "react";
+import { Link } from "gatsby";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
